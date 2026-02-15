49bbaaffaccf77283f175059d384543af0f60882
maxpain62 <maxpain62@gmail.com>
2026-02-15 10:37:52 +0530

    changed code for debug

--- src/main/java/com/org/proto/catalog/config/AwsS3ClientConfig.java
+++ src/main/java/com/org/proto/catalog/config/AwsS3ClientConfig.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 @Configuration
 public class AwsS3ClientConfig {
 
@@ -22,7 +25,7 @@ public class AwsS3ClientConfig {
     private String region;
 
     @Bean
-    public AmazonS3 s3client() {
+    /*public AmazonS3 s3client() {
 
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsId, awsKey);
         AmazonS3 amazonS3Client = AmazonS3ClientBuilder.standard()
@@ -31,5 +34,25 @@ public class AwsS3ClientConfig {
                 .build();
 
         return amazonS3Client;
+    }*/
+    public AmazonS3 s3client() {
+
+    try (FileWriter writer = new FileWriter("/tmp/aws-debug.txt", true)) {
+        writer.write("ACCESS KEY USED: [" + awsId + "]\n");
+        writer.write("SECRET KEY LENGTH: " + 
+            (awsKey != null ? awsKey.length() : 0) + "\n");
+        writer.write("REGION USED: " + region + "\n");
+        writer.write("----\n");
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+    BasicAWSCredentials awsCredentials =
+        new BasicAWSCredentials(awsId, awsKey);
+
+    return AmazonS3ClientBuilder.standard()
+            .withRegion(Regions.fromName(region))
+            .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+            .build();
     }
 }
